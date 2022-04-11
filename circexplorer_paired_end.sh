1abeda0a1ea2cb88e7957e18b414d2e84592a9c8
sambhav2mishra <52230097+sambhav2mishra@users.noreply.github.com>
2022-04-11 14:07:14 +0530

    Add files via upload

---
+++ circexplorer_paired_end.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+# Mouse
+mm39_ref_fasta=/path_to_your_annotation/ref_and_annotations/gencode_mm39/GRCm39.genome.fa
+mm39_kg_gtf=/path_to_your_annotationref_and_annotations/gencode_mm39/gencode.vM28.annotation.gtf
+mm39_star_index=/path_to_your_annotation/ref_and_annotations/gencode_mm39/star_index
+mm39_ref_all=/path_to_your_annotation/ref_and_annotations/gencode_mm39/gencode.vM28.annotation.txt
+
+
+##STAR alignment(paired-end)
+
+STAR_align=$(ls /path_to_your_fqfiles/fastq_files/*.fastq.gz | cut -d '/' -f 8 | cut -d '_' -f 1 | uniq); ## Please read about cut command first
+for DR in $STAR_align;
+do
+        FL1=$(ls /path_to_your_fqfiles/fastq_files/${DR}_1.fastq.gz) ##
+        FL2=$(ls /path_to_your_fqfiles/fastq_files/${DR}_2.fastq.gz) ##
+        STAR --runThreadN 30  --genomeDir ${mm39_star_index} --readFilesIn <(gunzip -c ${FL1} ${FL2}) --outSAMtype BAM Unsorted --sjdbGTFfile ${mm39_kg_gtf} --chimSegmentMin 10 --outFileNamePrefix ${DR}_
+done;
+echo "STAR alignment Completed successfully..."
+
+######
+parse=$(ls *Chimeric.out.junction | cut -d "_" -f 1 );   ## Please read about cut command first
+for i in $parse;
+do
+        CIRCexplorer2 parse -t STAR ${i}_Chimeric.out.junction > ${i}_CIRCexplorer2_parse.log
+        mv back_spliced_junction.bed ${i}_back_spliced_junction.bed
+done;
+echo "CIRCexplorer2 Parsing Completed successfully..."
+
+######
+annotate=$(ls *.bed | cut -d '_' -f 1); #Please read about cut command first
+echo "$annotate"
+for f in $annotate;
+do
+        CIRCexplorer2 annotate -r ${mm39_ref_all} -g ${mm39_ref_fasta} -b ${f}_back_spliced_junction.bed -o ${f}_circularRNA_known.txt > ${f}_CIRCexplorer2_annotate.log;
+done;
+echo "CIRCexplorer2 Annotation Completed successfully..."
